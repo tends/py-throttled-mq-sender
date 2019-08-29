db3036ec81d5a37139f68256a67d32a987d46ceb
Dmitriy Soldatov <soldatovdv@msk.bcs.ru>
2019-08-29 21:11:53 +0300

    Almost perfect xml_doc.py

--- xml_doc.py
+++ xml_doc.py
@@ -1,33 +1,35 @@
 import datetime
-from time import strftime, gmtime
-
 import xmltodict
 
 
 class XmlDoc(object):
-    # xml_dict = ""
     def __init__(self, file):
+        self.trade_id = ""
+
         with open(file) as fd:
             self.xml_dict = xmltodict.parse(fd.read())
 
         self.event_id = int(self.xml_dict['tradeEvent']['eventIdentifier']['eventId'])
+
         _tradeId = self.xml_dict['tradeEvent']['trade']['tradeIdentifier']['tradeId'].split(':')
         self.trade_id_prefix = _tradeId[0]
-        self.trade_id_num = int(_tradeId[1])
+        self._trade_id_num = int(_tradeId[1])
 
-    def get_trade_id(self):
-        return "{0}:{1}".format(self.trade_id_prefix, self.trade_id_num)
+    def get_xml(self):
+        self._trade_id_num = self._trade_id_num + 1
 
-    # def increment_numbers(self):
-
-    def get_doc(self):
         self.event_id = self.event_id + 1
-        self.trade_id_num = self.trade_id_num + 1
+        self.xml_dict['tradeEvent']['trade']['tradeIdentifier']['tradeId'] = \
+            "{0}:{1}".format(self.trade_id_prefix, self._trade_id_num)
+        _now = datetime.datetime.now()
 
-        _now = datetime.now()
+        self.xml_dict['tradeEvent']['eventIdentifier']['eventDateTime'] = \
+            self.xml_dict['tradeEvent']['trade']['tradeIdentifier']['tradeUpdateDateTime'] = \
+            self.xml_dict['tradeEvent']['trade']['tradeDetails']['tradeDateTime'] = _now.strftime("%Y-%m-%dT%H:%M:%S")
+
         _now_plus_2d = _now + datetime.timedelta(days=2)
+        self.xml_dict['tradeEvent']['trade']['tradeDetails']['equity']['settleDate'] = \
+            self.xml_dict['tradeEvent']['trade']['tradeDetails']['equity']['cashSettleDate'] = \
+            _now_plus_2d.strftime("%Y-%m-%d")
 
-        # TODO: strftime("%Y-%m-%dT%H:%M:%S", gmtime())
-
-
-        return xmltodict.unparse(self.xml_dict)
+        return xmltodict.unparse(self.xml_dict, pretty=True)

2d9da399134868822b431dbfb52f8c59772fca73
Dmitriy Soldatov <soldatovdv@msk.bcs.ru>
2019-08-29 22:07:24 +0300

    Refactor sender.py

--- sender.py
+++ sender.py
@@ -9,8 +9,8 @@ class MyListener(stomp.ConnectionListener):
     def on_error(self, headers, message):
         print('received an error "%s"' % message)
 
-    def on_message(self, headers, message):
-        print('received a message "%s"' % message)
+    # def on_message(self, headers, message):
+    #     print('received a message "%s"' % message)
 
 
 conn = stomp.Connection(auto_content_length=False)
@@ -19,20 +19,20 @@ conn.start()
 conn.connect('admin', 'password', wait=True)
 conn.subscribe(destination='/queue/test', id=1, ack='auto')
 
-
 xml_doc = XmlDoc('EquityNew - Copy (1).xml')
-print(xml_doc.event_id), exit()
 
 
 @RateLimited(100)
-def conn_send(body, destination):
-    conn.send(body=body, destination=destination)
+def conn_send(destination):
+    _xml = xml_doc.get_msg()
+    conn.send(body=_xml[1], destination=destination)
+    print("msg id: " + str(_xml[0]))
 
 
 start_time = time.time()
 
 for e in range(5000):
-    conn_send(body=str(e), destination='/queue/test')
+    conn_send(destination='/queue/test')
 
 stop_time = time.time()
 
@@ -40,10 +40,3 @@ time.sleep(2)
 print(stop_time - start_time)
 conn.disconnect()
 
-
-
-
-# if __name__ == '__main__':
-#     while True:
-#         time.sleep(5)
-#         conn.send_life_sign()

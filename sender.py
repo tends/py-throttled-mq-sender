76b3d766a9c167237e7da252f432cf1a494d8d16
Dmitriy Soldatov <soldatovdv@msk.bcs.ru>
2019-08-29 13:29:54 +0300

    Add throattling func

--- sender.py
+++ sender.py
@@ -1,29 +1,64 @@
-from amqstompclient import amqstompclient
-import logging
+
 import time
+from ratelimit import limits, sleep_and_retry
 import stomp
 
-stomp.Connection10
 
-def callback(destination, message, headers):
-    logger.info("Received:" + message)
-    conn.send_message("/topic/TTEST2", "FROMCALLBACK")
+class MyListener(stomp.ConnectionListener):
+    def on_error(self, headers, message):
+        print('received an error "%s"' % message)
+
+    def on_message(self, headers, message):
+        print('received a message "%s"' % message)
 
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
-                    datefmt="%Y-%m-%d %H:%M:%S")
-logger = logging.getLogger()
+conn = stomp.Connection()
+conn.set_listener('', MyListener())
+conn.start()
+conn.connect('admin', 'password', wait=True)
 
-server = {"ip": "127.0.0.1", "port": "61613", "login": "admin", "password": "admin", "heartbeats": (30000, 30000)}
-conn = amqstompclient.AMQClient(server
-                                , {"name": "TEST", "version": "1.0.0", "lifesign": "/topic/HELLO"}
-                                , ["/queue/QTEST1", "/queue/QTEST2", "/topic/HELLO"]
-                                , callback=callback)
+conn.subscribe(destination='/queue/test', id=1, ack='auto')
 
-conn.send_message("/topic/COUCOU", "WUT!!!")
+# conn.send(body=' '.join(sys.argv[1:]), destination='/queue/test')
 
-if __name__ == '__main__':
-    while True:
-        time.sleep(5)
-        conn.send_life_sign()
+
+def RateLimited(maxPerSecond):
+    minInterval = 1.0 / float(maxPerSecond)
+    def decorate(func):
+        lastTimeCalled = [0.0]
+        def rateLimitedFunction(*args,**kargs):
+            elapsed = time.perf_counter() - lastTimeCalled[0]
+            leftToWait = minInterval - elapsed
+            if leftToWait>0:
+                time.sleep(leftToWait)
+            ret = func(*args,**kargs)
+            lastTimeCalled[0] = time.perf_counter()
+            return ret
+        return rateLimitedFunction
+    return decorate
+
+# @sleep_and_retry
+# @limits(calls=20, period=1)
+@RateLimited(20)
+def conn_send(body, destination):
+    conn.send(body=body, destination=destination)
+
+
+start_time = time.time()
+
+for e in range(100):
+    conn_send(body=str(e), destination='/queue/test')
+
+stop_time = time.time()
+print(stop_time - start_time)
+time.sleep(2)
+
+conn.disconnect()
+
+
+
+
+# if __name__ == '__main__':
+#     while True:
+#         time.sleep(5)
+#         conn.send_life_sign()

297061c28560d8ebf9e6822839a61fcf0ce0b31a
Dmitriy Soldatov <soldatovdv@msk.bcs.ru>
2019-08-29 22:06:13 +0300

    Almost perfect rate_limit.py

--- rate_limit.py
+++ rate_limit.py
@@ -3,19 +3,21 @@ import time
 
 def RateLimited(maxPerSecond):
     minInterval = 1.0 / float(maxPerSecond)
-    def decorate(func):
-        lastTimeCalled = [0] #[0.0]
-        def rateLimitedFunction(*args,**kargs):
-            elapsed = lastTimeCalled[0]  # time.perf_counter() - lastTimeCalled[0]
 
-            leftToWait = minInterval - elapsed
-            if leftToWait>0:
+    def decorate(func):
+        elapsed = [0]  # [0.0]
+
+        def rateLimitedFunction(*args, **kargs):
+            leftToWait = minInterval - elapsed[0]
+            if leftToWait > 0:
                 time.sleep(leftToWait)
             start__time = time.perf_counter()
-            ret = func(*args,**kargs)
+            ret = func(*args, **kargs)
             stop__time = time.perf_counter()
 
-            lastTimeCalled[0] = stop__time - start__time  #time.perf_counter()
+            elapsed[0] = stop__time - start__time
             return ret
+
         return rateLimitedFunction
+
     return decorate
